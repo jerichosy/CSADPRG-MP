017e521e8ddba0a8680393cf3f889b89c1da4628
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-04-09 19:53:04 +0800

    Add PhilHealth tests and corrected the calc

--- calc/PhilHealth.go
+++ calc/PhilHealth.go
@@ -1,17 +1,21 @@
 package calc
 
+// 2022 PhilHealth Premium Rate
 // https://www.philhealth.gov.ph/news/2019/new_contri.php#gsc.tab=0
 // https://www.philhealth.gov.ph/advisories/2022/adv2022-0010.pdf
 func CalcPhilHealth(income float64) float64 {
 	var result float64 = 0
 
 	if income <= 10000 {
-		result = 200.00
+		result = 400.00
 	} else if income > 10000 && income < 80000 {
-		result = income * 0.02
+		result = income * 0.04
 	} else if income >= 80000 {
 		result = 3200.00
 	}
+
+	// Split between employer and employee
+	result = result / 2
 
 	return result
 }
---
+++ calc/PhilHealth_test.go
@@ -0,0 +1,34 @@
+package calc
+
+import "testing"
+
+type PhilHealthTest struct {
+	input    float64
+	expected float64
+}
+
+var philHealthTests = []PhilHealthTest{
+	{30000, 600},
+	{1000, 200},
+	{500000, 1600},
+	{1337, 200},
+	{4250, 200},
+	{1984, 200},
+	{33332, 666.64},
+	{33333, 666.66},
+	{9999.99, 200},
+	{10000, 200},
+	{10000.01, 200.0002},
+	{79999.99, 1599.9998},
+	{80000, 1600},
+	{80000.01, 1600},
+}
+
+func TestPhilHealth(t *testing.T) {
+	for _, test := range philHealthTests {
+		got := CalcPhilHealth(test.input)
+		if got != test.expected {
+			t.Errorf("CalcPhilHealth(%f) = %f; want %f", test.input, got, test.expected)
+		}
+	}
+}
