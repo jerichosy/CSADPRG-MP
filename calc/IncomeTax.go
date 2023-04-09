fe2550aa99827ce4fcf1db4bc2e67bbb78c7d859
kndone <justinlim4916@gmail.com>
2023-04-09 16:24:06 +0800

    Implemented Income Tax Calculation

---
+++ calc/IncomeTax.go
@@ -0,0 +1,19 @@
+package calc
+
+// https://www.bir.gov.ph/images/bir_files/internal_communications_2/RMCs/2018/WT%20table.pdf
+func CalcIncomeTax(taxableIncome float64) float64 {
+	switch {
+	case taxableIncome <= 20833:
+		return 0
+	case taxableIncome <= 33332:
+		return (taxableIncome - 20833) * 0.2
+	case taxableIncome <= 66666:
+		return (taxableIncome-33333)*0.25 + 2500
+	case taxableIncome <= 166666:
+		return (taxableIncome-66667)*0.3 + 10833.33
+	case taxableIncome <= 666666:
+		return (taxableIncome-166667)*0.32 + 40833.33
+	default:
+		return (taxableIncome-666667)*0.35 + 200833.33
+	}
+}
---
+++ calc/IncomeTax_test.go
@@ -0,0 +1,33 @@
+package calc
+
+import "testing"
+
+type IncomeTaxTest struct {
+	input    float64
+	expected float64
+}
+
+var incomeTaxTests = []IncomeTaxTest{
+	{0, 0.0},
+	{10000, 0.0},
+	{20833, 0.0},
+	{30000, 1833.4},
+	{33332, 2499.8},
+	{33333, 2500.0},
+	{50000, 6666.75},
+	{66666, 10833.25},
+	{100000, 20833.23},
+	{166666, 40833.03},
+	{500000, 147499.89},
+	{666666, 200833.01},
+	{1000000, 317499.88},
+}
+
+func TestIncomeTax(t *testing.T) {
+	for _, test := range incomeTaxTests {
+		got := CalcIncomeTax(test.input)
+		if got != test.expected {
+			t.Errorf("CalcSSS(%f) = %f; want %f", test.input, got, test.expected)
+		}
+	}
+}
