cf05476b23893e94eda346d9a8daa2069c5e9409
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-04-09 17:28:40 +0800

    Forgot to format SSS_test.go to indent using 4 spaces

--- calc/SSS_test.go
+++ calc/SSS_test.go
@@ -3,28 +3,28 @@ package calc
 import "testing"
 
 type sssTest struct {
-	input float64
-	expected  float64
+    input float64
+    expected  float64
 }
 
 var sssTests = []sssTest{
-	sssTest{999.99, 0},
-	sssTest{1000, 135},
-	sssTest{4249.99, 180},
-	sssTest{4250, 202.5},
-	sssTest{6250, 292.5},
-	sssTest{7000, 315},
-	sssTest{8249.99, 360},
-	sssTest{24749.99, 1102.5},
-	sssTest{24750, 1125},
-	sssTest{500000, 1125},
+    sssTest{999.99, 0},
+    sssTest{1000, 135},
+    sssTest{4249.99, 180},
+    sssTest{4250, 202.5},
+    sssTest{6250, 292.5},
+    sssTest{7000, 315},
+    sssTest{8249.99, 360},
+    sssTest{24749.99, 1102.5},
+    sssTest{24750, 1125},
+    sssTest{500000, 1125},
 }
 
 func TestSSS(t *testing.T) {
-	for _, test := range sssTests {
-		got := CalcSSS(test.input)
-		if got != test.expected {
-			t.Errorf("CalcSSS(%f) = %f; want %f", test.input, got, test.expected)
-		}
-	}
+    for _, test := range sssTests {
+        got := CalcSSS(test.input)
+        if got != test.expected {
+            t.Errorf("CalcSSS(%f) = %f; want %f", test.input, got, test.expected)
+        }
+    }
 }
